89f50175ebe33e696a84faa25df32bc5985fc65f
VioletMistCloudDrift <violetmistclouddrift@outlook.com>
2023-10-23 09:53:06 +0800

    :tada: 1.20.1

--- src/main/java/neko/violetmist/enumcache/mixin/mc/common/MixinItemDisplayContext.java
+++ src/main/java/neko/violetmist/enumcache/mixin/mc/common/MixinItemDisplayContext.java
@@ -1,17 +1,17 @@
 package neko.violetmist.enumcache.mixin.mc.common;
 
 import neko.violetmist.enumcache.api.ICacheableEnum;
-import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket;
+import net.minecraft.world.item.ItemDisplayContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ClientboundPlayerPositionPacket.RelativeArgument.class)
-public abstract class MixinClientboundPlayerPositionPacket$RelativeArgument implements ICacheableEnum {
+@Mixin(ItemDisplayContext.class)
+public abstract class MixinItemDisplayContext implements ICacheableEnum {
     @Unique
-    private static ClientboundPlayerPositionPacket.RelativeArgument[] enumCache$cache = null;
+    private static ItemDisplayContext[] enumCache$cache = null;
 
     @Override
     public void enumCache$clearValuesCache() {
@@ -19,12 +19,12 @@ public abstract class MixinClientboundPlayerPositionPacket$RelativeArgument impl
     }
 
     @Inject(method = "values", at = @At("HEAD"), cancellable = true)
-    private static void enumCache$inject$values$head(CallbackInfoReturnable<ClientboundPlayerPositionPacket.RelativeArgument[]> cir) {
+    private static void enumCache$inject$values$head(CallbackInfoReturnable<ItemDisplayContext[]> cir) {
         if (enumCache$cache != null) cir.setReturnValue(enumCache$cache);
     }
 
     @Inject(method = "values", at = @At("RETURN"))
-    private static void enumCache$inject$values$tail(CallbackInfoReturnable<ClientboundPlayerPositionPacket.RelativeArgument[]> cir) {
+    private static void enumCache$inject$values$tail(CallbackInfoReturnable<ItemDisplayContext[]> cir) {
         if (enumCache$cache == null) enumCache$cache = cir.getReturnValue();
     }
 }
