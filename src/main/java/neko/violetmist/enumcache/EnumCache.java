c4aab603b12c6b29f5c6649530c018208c66268a
VioletMistCloudDrift <violetmistclouddrift@outlook.com>
2023-10-23 08:46:15 +0800

    :tada: Add EnumCache#clearCacheForEnum

--- src/main/java/neko/violetmist/enumcache/EnumCache.java
+++ src/main/java/neko/violetmist/enumcache/EnumCache.java
@@ -1,8 +1,22 @@
 package neko.violetmist.enumcache;
 
+import neko.violetmist.enumcache.api.ICacheableEnum;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(EnumCache.ID)
 public class EnumCache {
     public static final String ID = "enumcache";
+
+    public static <T extends Enum<T>> boolean clearCacheForEnum(Class<T> clazz) {
+        if (!clazz.isEnum()) return false;
+        T[] values = clazz.getEnumConstants();
+        if (values == null || values.length == 0) return false;
+        T instance = values[0];
+        if (instance instanceof ICacheableEnum) {
+            final ICacheableEnum cacheable = (ICacheableEnum) instance;
+            cacheable.enumCache$clearValuesCache();
+            return true;
+        }
+        return false;
+    }
 }
