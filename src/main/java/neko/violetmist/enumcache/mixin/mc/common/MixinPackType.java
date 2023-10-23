849bae017a37e207c51ef8eb21713dfbeecbf403
VioletMistCloudDrift <violetmistclouddrift@outlook.com>
2023-10-23 09:31:30 +0800

    :bug: Fix missing mixins

---
+++ src/main/java/neko/violetmist/enumcache/mixin/mc/common/MixinPackType.java
@@ -0,0 +1,30 @@
+package neko.violetmist.enumcache.mixin.mc.common;
+
+import neko.violetmist.enumcache.api.ICacheableEnum;
+import net.minecraft.server.packs.PackType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PackType.class)
+public abstract class MixinPackType implements ICacheableEnum {
+    @Unique
+    private static PackType[] enumCache$cache = null;
+
+    @Override
+    public void enumCache$clearValuesCache() {
+        enumCache$cache = null;
+    }
+
+    @Inject(method = "values", at = @At("HEAD"), cancellable = true)
+    private static void enumCache$inject$values$head(CallbackInfoReturnable<PackType[]> cir) {
+        if (enumCache$cache != null) cir.setReturnValue(enumCache$cache);
+    }
+
+    @Inject(method = "values", at = @At("RETURN"))
+    private static void enumCache$inject$values$tail(CallbackInfoReturnable<PackType[]> cir) {
+        if (enumCache$cache == null) enumCache$cache = cir.getReturnValue();
+    }
+}
